e5a1e2197ad271957f9db9af96d6bcc1be2ab3a2
Rane Wallin <RAN2143066@maricopa.edu>
2019-01-18 07:50:23 -0700

    callbacks (plus stretch)

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,24 +24,38 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+    return cb(arr.length);
 }
+
+getLength([1, 2, 3], length => console.log(`The array has ${length} elements.`));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+    return cb(arr[arr.length-1]);
 }
+last([1, 2, 3, 4], elm => console.log(`The last element is ${elm}.`) );
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+    return cb(x+y);
 }
+sumNums(3, 3, sum => console.log(`The sum is ${sum}.`));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+    return cb(x*y);
 }
+multiplyNums(2, 3, product => console.log(`The product is ${product}.`));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+    return cb(list.includes(item));
 }
+//true
+contains(2, [1, 2, 3, 4], result => console.log(`The item ${result? 'is':'is not'} in the list.`));
+//false
+contains(6, [1, 2, 3, 4], result => console.log(`The item ${result? 'is':'is not'} in the list.`));
 
 /* STRETCH PROBLEM */
 
@@ -49,4 +63,9 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+    let noDupes = [];
+    array.forEach(elm => noDupes.includes(elm) ? null : noDupes.push(elm));
+    return cb(noDupes);
 }
+
+removeDuplicates([1, 2, 1, 4, 5, 6, 7, 3, 4, 6, 9], result => console.log(`No duplicates: ${result}.`));
