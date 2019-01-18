5160355226051a536a1eab0cedf8ccd0d69cd87d
Rane Wallin <RAN2143066@maricopa.edu>
2019-01-18 07:58:42 -0700

    stretch: counter factory

--- assignments/closure.js
+++ assignments/closure.js
@@ -27,5 +27,17 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+    let count = 0;
+    return {
+        increment: () => count++,
+        decrement: () => count--
+    }
 };
+
+let counter2 = counterFactory();
+console.log(counter2.increment());
+console.log(counter2.decrement());
+console.log(counter2.increment());
+console.log(counter2.increment());
+console.log(counter2.increment());
              
